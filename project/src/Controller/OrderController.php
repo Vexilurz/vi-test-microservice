f2dc325709cf72984d21536020099493ea2eacab
Sergei Potekhin <vexilurz@gmail.com>
2021-10-22 14:52:14 +0300

    order/add_product route

--- project/src/Controller/OrderController.php
+++ project/src/Controller/OrderController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Repository\OrderRepository;
+use App\Repository\ProductRepository;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,7 +19,8 @@ class OrderController extends AbstractController
     /**
      * @Route("/create", name="order_create")
      */
-    public function create(Request $request, OrderRepository $orderRepository, UserRepository $userRepository): Response
+    public function create(Request $request, OrderRepository $orderRepository,
+                           UserRepository $userRepository): Response
     {
         if (!$request->isMethod('POST')) {
             return $this->json(['message'=>'Must be a POST method'], Response::HTTP_BAD_REQUEST);
@@ -30,6 +33,45 @@ class OrderController extends AbstractController
         return $this->json([
             'message' => 'new order created',
             'id' => $order->getId()
+        ]);
+    }
+
+    /**
+     * @Route("/add_product", name="order_add_product")
+     */
+    public function addProduct(Request $request, OrderRepository $orderRepository,
+                               ProductRepository $productRepository,
+                               UserRepository $userRepository,
+                               EntityManagerInterface $entityManager): Response
+    {
+        if (!$request->isMethod('POST')) {
+            return $this->json(['message'=>'Must be a POST method'], Response::HTTP_BAD_REQUEST);
+        }
+
+        $apiToken = $request->headers->get('X-AUTH-TOKEN');
+        $userFromToken = $userRepository->findOneBy(['apiToken' => $apiToken]);
+
+        $orderId = $request->request->get('orderId', 0);
+        $productId = $request->request->get('productId', 0);
+        $order = $orderRepository->find($orderId);
+        $product = $productRepository->find($productId);
+
+        if (!$order || !$product) {
+            return $this->json(['message'=>'order or product not found'], Response::HTTP_BAD_REQUEST);
+        }
+
+        if ($order->getUser() !== $userFromToken) {
+            return $this->json(
+                ['message'=>'user from apiToken are not the owner of that order'],
+                Response::HTTP_FORBIDDEN
+            );
+        }
+
+        $order->addProduct($product);
+        $entityManager->flush();
+
+        return $this->json([
+            'message' => 'product added to order'
         ]);
     }
 }
