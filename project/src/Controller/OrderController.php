bc8552907231eb9a4877ad64552831a7f2f3ba65
Sergei Potekhin <vexilurz@gmail.com>
2021-10-22 15:32:46 +0300

    order/remove_product route. Need refactor

--- project/src/Controller/OrderController.php
+++ project/src/Controller/OrderController.php
@@ -74,4 +74,43 @@ class OrderController extends AbstractController
             'message' => 'product added to order'
         ]);
     }
+
+    /**
+     * @Route("/remove_product", name="order_remove_product")
+     */
+    public function removeProduct(Request $request, OrderRepository $orderRepository,
+                               ProductRepository $productRepository,
+                               UserRepository $userRepository,
+                               EntityManagerInterface $entityManager): Response
+    {
+        if (!$request->isMethod('POST')) {
+            return $this->json(['message'=>'Must be a POST method'], Response::HTTP_BAD_REQUEST);
+        }
+
+        $apiToken = $request->headers->get('X-AUTH-TOKEN');
+        $userFromToken = $userRepository->findOneBy(['apiToken' => $apiToken]);
+
+        $orderId = $request->request->get('orderId', 0);
+        $productId = $request->request->get('productId', 0);
+        $order = $orderRepository->find($orderId);
+        $product = $productRepository->find($productId);
+
+        if (!$order || !$product) {
+            return $this->json(['message'=>'order or product not found'], Response::HTTP_BAD_REQUEST);
+        }
+
+        if ($order->getUser() !== $userFromToken) {
+            return $this->json(
+                ['message'=>'user from apiToken are not the owner of that order'],
+                Response::HTTP_FORBIDDEN
+            );
+        }
+
+        $order->removeProduct($product);
+        $entityManager->flush();
+
+        return $this->json([
+            'message' => 'product removed from order'
+        ]);
+    }
 }
