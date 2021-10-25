d9c17c7bf63a13fe4ac7723b501a4da12b30dc8e
Sergei Potekhin <vexilurz@gmail.com>
2021-10-25 10:46:42 +0300

    Update fixtures

---
+++ project/src/DataFixtures/AppFixtures.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Order;
+use App\Entity\Product;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    private $encoder;
+    private $em;
+
+    public function __construct(UserPasswordHasherInterface $encoder, EntityManagerInterface $entityManager)
+    {
+        $this->encoder = $encoder;
+        $this->em = $entityManager;
+    }
+
+    private function prepareUsers(): void
+    {
+        $usersData = [
+            [
+                'email' => 'user@example.com',
+                'password' => 123456
+            ],
+            [
+                'email' => 'user2@example.com',
+                'password' => 111111
+            ],
+            [
+                'email' => 'user3@example.com',
+                'password' => 222222
+            ]
+        ];
+        foreach ($usersData as $user) {
+            $newUser = new User();
+            $newUser->setEmail($user['email']);
+            $newUser->setPassword($this->encoder->hashPassword($newUser, $user['password']));
+            $newUser->setRoles(['ROLE_USER']);
+            $this->em->persist($newUser);
+        }
+    }
+
+    private function prepareProducts() {
+        $productsData = [
+            [
+                'name' => 'Microphone',
+                'price' => 12400,
+                'available' => true,
+            ],
+            [
+                'name' => 'Guitar',
+                'price' => 35125,
+                'available' => false,
+            ],
+            [
+                'name' => 'Keyboard',
+                'price' => 8145,
+                'available' => true,
+            ],
+        ];
+        foreach ($productsData as $product) {
+            $newProduct = new Product();
+            $newProduct->setName($product['name']);
+            $newProduct->setPrice($product['price']);
+            $newProduct->setAvailable($product['available']);
+            $dateTime = new \DateTimeImmutable('now');
+            $newProduct->setCreatedAt($dateTime);
+            $newProduct->setUpdatedAt($dateTime);
+            $this->em->persist($newProduct);
+        }
+    }
+
+    private function prepareOrders() {
+        $user = $this->em->getRepository(User::class)->findOneBy(['email' => 'user@example.com']);
+        $productsRepository = $this->em->getRepository(Product::class);
+        $products[] = $productsRepository->findOneBy(['name' => 'Microphone']);
+        $products[] = $productsRepository->findOneBy(['name' => 'Guitar']);
+        $products[] = $productsRepository->findOneBy(['name' => 'Keyboard']);
+
+        $newOrder = new Order();
+        $newOrder->setUser($user);
+        $newOrder->setPaid(true);
+        $newOrder->setTotalPrice(0);
+        $newOrder->addProduct($products[0]);
+        $newOrder->addProduct($products[1]);
+        $dateTime = new \DateTimeImmutable('now - 15 days');
+        $newOrder->setCreatedAt($dateTime);
+        $newOrder->setUpdatedAt($dateTime);
+        $this->em->persist($newOrder);
+
+        $newOrder = new Order();
+        $newOrder->setUser($user);
+        $newOrder->setPaid(false);
+        $newOrder->setTotalPrice(0);
+        $dateTime = new \DateTimeImmutable('now - 10 day');
+        $newOrder->setCreatedAt($dateTime);
+        $newOrder->setUpdatedAt($dateTime);
+        $this->em->persist($newOrder);
+
+        $newOrder = new Order();
+        $newOrder->setUser($user);
+        $newOrder->setPaid(false);
+        $newOrder->setTotalPrice(0);
+        $newOrder->addProduct($products[1]);
+        $newOrder->addProduct($products[2]);
+        $dateTime = new \DateTimeImmutable('now - 5 days');
+        $newOrder->setCreatedAt($dateTime);
+        $newOrder->setUpdatedAt($dateTime);
+        $this->em->persist($newOrder);
+    }
+
+    public function load(\Doctrine\Persistence\ObjectManager $manager)
+    {
+        $this->prepareUsers();
+        $this->prepareProducts();
+        $this->em->flush();
+        $this->prepareOrders();
+        $this->em->flush();
+    }
+}
